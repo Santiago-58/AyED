0d210eb4db0f6e7c1d9ec4478db9e6560d916354
Santiago Luis Aliaga Choque <aliagasantiago58@gmail.com>
2025-05-08 23:34:43 -0300

    readme.md

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hola Santiago Luis Aliaga Choque" << std::endl;
+    std::cout << "Hola Santiago Luis Aliaga Choque, g++ (Rev2, Built by MSYS2 project) 14.2.0" << std::endl;
     return 0;
 }
