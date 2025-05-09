ab36ca1173e4161d9c3dd32ca59f3b4846dcaac8
Santiago Luis Aliaga Choque <aliagasantiago58@gmail.com>
2025-05-08 23:31:31 -0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 # AyED
-readme.md // Santiago Luis Aliaga Choque K1043
-main.cpp // g++ (Rev2, Built by MSYS2 project) 14.2.0
-Copyright (C) 2024 Free Software Foundation, Inc.
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Santiago Luis Aliaga Choque" << std::endl;
+    return 0;
+}
